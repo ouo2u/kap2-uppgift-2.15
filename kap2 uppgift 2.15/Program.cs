b8751306b799aae68a1d3b92a51474bee16fcc5b
ouo2u <abod98451@gmail.com>
2022-09-13 21:48:13 +0200

    Add project files.

---
+++ kap2 uppgift 2.15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace kap2_uppgift_2._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv ett ord");
+            string ord1 = Console.ReadLine();
+            Console.WriteLine("skriv ett ord till");
+            string ord2 = Console.ReadLine();
+            Console.WriteLine("skriv sista ord");
+            string ord3 = Console.ReadLine();
+
+            Console.WriteLine(ord3 + ord2 + ord1 +".");
+
+
+
+        }
+    }
+}
